78f16c61b4af8ab280e4ff49f06cbe9651be5d8e
brendon.caulkins <brendon.caulkins@gmail.com>
2019-02-12 18:20:13 -0500

    Rename AppComponent to HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { LoginComponent } from './login/login.component';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { NgModule } from '@angular/core'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  declarations: [HomeComponent, LoginComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,33 +1,33 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { TestBed, async } from '@angular/core/testing'
 
-import { AppComponent } from './app.component'
-import { AuthenticationService } from './services/authentication/authentication.service'
-import { LoginComponent } from './login/login.component'
+import { AuthenticationService } from '../services/authentication/authentication.service'
+import { HomeComponent } from './home.component'
+import { LoginComponent } from '../login/login.component'
 
-describe('AppComponent', () => {
+describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent, LoginComponent],
+      declarations: [HomeComponent, LoginComponent],
       imports: [FormsModule, ReactiveFormsModule],
       providers: [AuthenticationService]
     }).compileComponents()
   }))
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
+    const fixture = TestBed.createComponent(HomeComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
   })
 
   it(`should have as title 'code-coverage-conspiracy'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
+    const fixture = TestBed.createComponent(HomeComponent)
     const app = fixture.debugElement.componentInstance
     expect(app.title).toEqual('code-coverage-conspiracy')
   })
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent)
+    const fixture = TestBed.createComponent(HomeComponent)
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h1').textContent).toContain(
