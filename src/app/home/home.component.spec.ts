d5643276c254842cb5cd0892721cce89e5c79215
brendon.caulkins <brendon.caulkins@gmail.com>
2019-02-12 22:40:27 -0500

    Add HomeComponent tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,15 +1,37 @@
 import { AuthenticationService } from '../services/authentication/authentication.service'
+import { Conspiracy } from '../models/conspiracy.model'
 import { HomeComponent } from './home.component'
+import { Subject } from 'rxjs'
 
 describe('HomeComponent', () => {
   let component: HomeComponent
+  let authService: AuthenticationService
   beforeEach(() => {
-    component = new HomeComponent(
-      new AuthenticationService() // Normally this would be a mock service
-    )
+    authService = new AuthenticationService() // Normally this would be a mock service
+    spyOn(authService, 'logout').and.callThrough()
+    component = new HomeComponent(authService)
   })
 
   it('should create the app', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should store a title', () => {
+    expect(component.title).toBe('code-coverage-conspiracy')
+  })
+
+  it('should support edit events', () => {
+    expect(component.editConspiracy instanceof Subject).toBe(true)
+  })
+
+  it('should keep track of the logged in status', done => {
+    component.loggedIn.subscribe(status => {
+      expect(status).toBe(false) // The default status on startup
+      done()
+    })
+  })
+
+  it('should call logout()', () => {
+    expect(authService.logout).toHaveBeenCalled()
   })
 })
